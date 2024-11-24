e9f47024d27f54203f4c77c70457b71ec0f2a65d
Alex.sanchez <alex.sanchez.alex2.0@gmail.com>
2024-11-24 10:50:13 +0500

    Add files via upload

--- main7.py
+++ main7.py
@@ -1,11 +1,101 @@
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+while(True):
+    print('1. Add website')
+    print('2. Find Keyword')
+    print('3. Delete Website')
+    print('4. Exit Website')
+    command = int(input())
+    if(command == 1):
+        print("Please input the domain of website with https")
+        url = input()
+        connection = sqlite3.connect('main.sl3')
+        cur = connection.cursor()
+        cur.execute('Create table if not exists website(name text)')
+        cur.execute("insert into website(name) values(?)", (url,))
+        connection.commit()
+        connection.close()
+        print("success")
+    elif(command == 2):
+        print("Please input the domain of website with https")
+        url = input()
+        print("Please input the keyword from website")
+        key = input()
+        connection = sqlite3.connect('main.sl3')
+        cur = connection.cursor()
+        cur.execute('select * from website where name = ?', (url,))
+        row = cur.fetchall()
+        if row:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            text = soup.get_text()
+            cnt = text.lower().count(key.lower())
+            print('Found ' + str(cnt) + ' keywords.')
+        else:
+            print("No such website is in the Database")
+        connection.commit()
+        connection.close()
+        print("success")
+    elif(command == 3):
+        print(command)
+    else:
+        break
+import sqlite3
+from bs4 import BeautifulSoup
+import requests
 
-n = int(input("число: "))
+while(True):
+    print("1. Add website")
+    print("2. Find Keyword")
+    print("3. Delete Website")
+    print("4. Exit Website")
+    command = int(input())
+    if(command == 1):
+        print("Please input the name of website with https")
+        url = input()
+        connection = sqlite3.connect('main.sl3')
+        cur = connection.cursor()
+        cur.execute("Create table if not exists website(name text)")
+        cur.execute("insert into website(name) values (?)", (url,))
+        connection.commit()
+        connection.close()
+        print("success!")
 
-def van(n):
-   for i in range (n+1):
-      yield i
-
-fun = van(n)
-for i in fun:
-      
-   print(i)
+    elif(command == 2):
+        print("Please input the name of website with https")
+        url = input()
+        print("Please input the keyword from website")
+        key = input()
+        connection = sqlite3.connect('main.sl3')
+        cur = connection.cursor()
+        cur.execute('select * from website where name = ?', (url,))
+        row = cur.fetchall()
+        if row:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            text = soup.get_text()
+            keyword_count = text.lower().count(key.lower())
+            print("Found " + str(keyword_count) + " keywords.")
+        else:
+            print("No such website is in the Database")
+        connection.commit()
+        connection.close()
+        
+    elif(command == 3):
+        print("Please input the name of website with https")
+        url = input()
+        connection = sqlite3.connect('main.sl3')
+        cur = connection.cursor()
+        cur.execute('select * from website where name = ?', (url,))
+        row = cur.fetchall()
+        if row:
+            cur.execute('delete from website where name = ?', (url,))
+            print("Deleted!")
+        else:
+            print("No such website is in the Database")
+        connection.commit()
+        connection.close()
+    else:
+        print("Goodbye!")
+        break
